97d7fc0abfc8265dc3249a182ea841d036f2c940
carlosperate <carlosperate@users.noreply.github.com>
2015-04-20 20:24:18 +0100

    Add prof pic, organise pelican config and CSS

--- embeddedlog/pelicanconf.py
+++ embeddedlog/pelicanconf.py
@@ -5,65 +5,80 @@ from __future__ import unicode_literals
 # This variable is used for debug purposes
 DEPLOY_RUN = False
 
-AUTHOR = u'Carlos Perate'
+# Website data
+AUTHOR = u'carlosperate'
 SITENAME = u'Embedded Log'
+GOOGLE_ANALYTICS = 'UA-58081399-1'
+TIMEZONE = 'Europe/London'
+DEFAULT_LANG = u'en'
+CC_LICENSE = 'CC-BY'
+
+# Paths data
 if DEPLOY_RUN is True:
     SITEURL = 'http://www.embeddedlog.com'
 else:
     SITEURL = ''
-GOOGLE_ANALYTICS = 'UA-58081399-1'
-
+    RELATIVE_URLS = True
 PATH = 'content'
 STATIC_PATHS = ['images']
 
-PAGE_PATHS = ['pages']
-PAGE_URL = '{slug}/index.html'
-PAGE_SAVE_AS = '{slug}/index.html'
-PAGE_ORDER_BY = 'page-order'
-
-#CATEGORY_URL = '{category}/{slug}.html'
-#CATEGORY_SAVE_AS = '{category}/{slug}.html'
-
-DEFAULT_PAGINATION = 5
-
-TIMEZONE = 'Europe/London'
-DEFAULT_LANG = u'en'
-
-LOAD_CONTENT_CACHE = False
-
-DISPLAY_BREADCRUMBS = True
-
-SHARIFF = True
-SHARIFF_LANG = 'en'
-SHARIFF_SERVICES = '[&quot;twitter&quot;,&quot;facebook&quot;,&quot;googleplus&quot;,&quot;xing&quot;,&quot;whatsapp&quot;]'
-
-USE_FOLDER_AS_CATEGORY = True
-DEFAULT_CATEGORY = 'blog'
-
-SUMMARY_MAX_LENGTH = 150
-
-BANNER_ALL_PAGES = True
-BANNER = 'images/banner.jpg'
-BANNER_SUBTITLE = 'This is my subtitle'
-
 # Menu
 DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = True
-MENUITEMS = (('About', 'http://www.embeddedlog.com/about/index.html'),)
+DISPLAY_CATEGORIES_ON_MENU = False
+MENUITEMS = (('Blog', 'http://www.embeddedlog.com/category/blog.html'),
+             ('About', 'http://www.embeddedlog.com/about/index.html'),)
 
 # Sidebar
-DISPLAY_CATEGORIES_ON_SIDEBAR = True
+DISPLAY_CATEGORIES_ON_SIDEBAR = False
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 DISPLAY_TAGS_ON_SIDEBAR = True
 TAG_CLOUD_MAX_ITEMS = 10
+#MENUITEMS=(('', '#'))
 SOCIAL = (('github', 'https://github.com/carlosperate'),
           ('twitter', 'https://twitter.com/carlosperate'),
           ('google+', 'https://plus.google.com/109111328639820363664'),)
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),)
-#MENUITEMS=(('', '#'))
-TWITTER_USERNAME = 'carlosperate'
+
+
+# Pages data
+PAGE_PATHS = ['pages']
+PAGE_URL = '{slug}/index.html'
+PAGE_SAVE_AS = '{slug}/index.html'
+PAGE_ORDER_BY = 'page-order'
+
+# Categories data
+#CATEGORY_URL = '{category}/{slug}.html'
+#CATEGORY_SAVE_AS = '{category}/{slug}.html'
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'Blog'
+SUMMARY_MAX_LENGTH = 150
+
+# Both pages and categories
+BANNER_ALL_PAGES = True
+BANNER = 'images/banner.jpg'
+BANNER_SUBTITLE = 'This is my subtitle'
+
+# General navigation
+DEFAULT_PAGINATION = 5
+LOAD_CONTENT_CACHE = False
+DISPLAY_BREADCRUMBS = True
+
+# Share buttons
+SHARIFF = True
+SHARIFF_LANG = 'en'
+SHARIFF_SERVICES = '[&quot;twitter&quot;,&quot;facebook&quot;,&quot;googleplus&quot;,&quot;xing&quot;,&quot;whatsapp&quot;]'
+
+# Twitter Cards
+USE_OPEN_GRAPH = True
+TWITTER_CARDS = False
+TWITTER_USERNAME = None
+
+# Plugins
+PLUGIN_PATHS = ["plugins", "../plugins"]
+PLUGINS = ["related_posts", "pelican_youtube"]
+RELATED_POSTS_MAX = 5
 
 # Theme options
 THEME = '../themes/embeddedlog-theme'
@@ -72,19 +87,18 @@ GITHUB_USER = 'carlosperate'
 GITHUB_REPO_COUNT = 5
 GITHUB_SKIP_FORK = True
 GITHUB_SHOW_USER_LINK = True
+TWITTER_WIDGET_ID = None
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-
-# Plugins
-PLUGIN_PATHS = ["plugins", "../plugins"]
-PLUGINS = ["related_posts", "pelican_youtube"]
-
-RELATED_POSTS_MAX = 5
+if DEPLOY_RUN is True:
+    FEED_ALL_ATOM = None
+    CATEGORY_FEED_ATOM = None
+    TRANSLATION_FEED_ATOM = None
+    AUTHOR_FEED_ATOM = None
+    AUTHOR_FEED_RSS = None
+else:
+    FEED_ALL_ATOM = None
+    CATEGORY_FEED_ATOM = None
+    TRANSLATION_FEED_ATOM = None
+    AUTHOR_FEED_ATOM = None
+    AUTHOR_FEED_RSS = None
