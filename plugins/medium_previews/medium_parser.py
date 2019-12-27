f6650fe0225b80b27e316d4be490528fa38d5024
carlosperate <carlosperate@users.noreply.github.com>
2019-12-27 02:48:02 +0000

    Plugin: Update Medium Preview RSS parser for better summaries.
    By default the RSS summary is very large and contains the content
    HTML. This extracts the text and slices its length.

--- plugins/medium_previews/medium_parser.py
+++ plugins/medium_previews/medium_parser.py
@@ -3,10 +3,7 @@
 Retrieves Medium articles via RSS using FeedParser
 """
 from abc import ABCMeta, abstractmethod
-import json
-
-import feedparser
-import requests
+from importlib import import_module
 
 
 class MediumParser(object):
@@ -15,6 +12,16 @@ class MediumParser(object):
     def __init__(self, username):
         self.username = username
         self.medium_url = 'https://www.medium.com/@{}'.format(self.username)
+        self.extra_imports()
+
+    def _import(self, module_name):
+        globals()[module_name] = __import__(module_name)
+
+    def extra_imports(self):
+        """We only use one of these at a time, so to save load-time we only
+        import the additional dependencies needed during class initialisation.
+        """
+        pass
 
     @abstractmethod
     def get_preview_content(self):
@@ -59,15 +66,29 @@ class MediumParserRss(MediumParser):
     Retrieves Medium articles via RSS using FeedParser.
     """
 
+    def extra_imports(self):
+        self._import('feedparser')
+        self._import('html2text')
+
     def get_preview_content(self):
         medium_rss_url = 'https://medium.com/feed/@{}'.format(self.username)
         parsed_feed = feedparser.parse(medium_rss_url)
+        # RSS summary comes with all the post HTML, so we'll extract text
+        text_maker = html2text.HTML2Text()
+        text_maker.ignore_links = True
+        text_maker.bypass_tables = True
+        text_maker.ignore_images = True
+        text_maker.ignore_emphasis = True
         parsed_data = []
         for post in parsed_feed.entries:
+            short_summary = text_maker.handle(post['summary'])
+            # The summary is the majority of blog post content, cap it
+            if len(short_summary) > 250:
+                short_summary = short_summary[:250].rsplit(" ", 1)[0] + '...'
             parsed_data.append({
                 'title': post['title'],
                 'thumbnailUrl': '',
-                'summary': post['summary'],
+                'summary': short_summary,
                 'url': post['link']
             })
         return parsed_data
@@ -82,6 +103,10 @@ class MediumParserJson(MediumParser):
         super().__init__(*args, **kwargs)
         self.medium_json_url = 'https://medium.com/@{}/latest?format=json'.format(self.username)
         self.json_escape_str = '])}while(1);</x>'
+
+    def extra_imports(self):
+        self._import('json')
+        self._import('requests')
 
     def get_preview_content(self):
         json_str = requests.get(self.medium_json_url)
